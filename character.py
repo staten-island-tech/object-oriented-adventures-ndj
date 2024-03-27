5f6929b6b5819241365b36dcdd5a3b5fe889c23a
NicholasB09 <158211547+NicholasB09@users.noreply.github.com>
2024-03-27 13:58:31 -0400

    qwer

---
+++ character.py
@@ -0,0 +1,9 @@
+class Character():
+
+    def __init__(self, name, character_type, age):
+        self.name = name
+        self.type = character_type
+        self.age = age 
+    
+    def character_type(self):
+        if self.type 
