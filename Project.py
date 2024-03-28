b6ff7a9b52adbc04077dd39557e57ed5874a5045
JustinBregu <justinb612@nycstudents.net>
2024-03-28 14:00:20 -0400

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,7 +1,22 @@
-import random
-
 name = input("Enter your characters name: ")
-age = int(input("Enter your characters starting age:"))
-number = random.randomint(1, 18)
+age = int(input("Enter your characters starting age: (1,18)"))
 print(name)
 print(age)
+def colonies():
+    colony = input("Enter the colony you want to start off at:(North America/South America/Europe/Africa/Asia/Australia) ")
+    for colony in colonies:
+        if colony== "North America":
+            print(colony)
+            print("Welcome to your new adventure!")
+        if colony == "South America":
+            print(colony)
+            print("Good luck escaping the walls!")
+        if colony == "Europe":
+            print("Get geared up for your new journey!")
+        if colony == "Africa":
+            print("Don't try and become dehydrated!")
+        if colony == "Asia":
+            print("Welcome to the largest continent in the world!")
+        if colony == "Australia":
+            print("Don't let the kangaroos hurt you!")
+colonies()
