1a799d8157f65b117726512ded410ee065f44f69
DanielT09 <158211608+DanielT09@users.noreply.github.com>
2024-04-03 13:36:29 -0400

    po[]

--- Boss.py
+++ Boss.py
@@ -19,14 +19,27 @@ class Enemies():
 
 
 f = open("enemies.json",)
-data = json.load(f)
+enemies = json.load(f)
     ##Call classes in here
 
-def new_Enemies():
-    Name = input("Enter an Enemies Name: ")
-    health = int(input("Enter their health: "))
-    attack = int(input("Enter their attack: "))
-    x = Enemies(Name, health, attack)
-    data.append(x.dictionary())
 
-new_Enemies()
+Name = input("Enter an Enemies Name: ")
+health = int(input("Enter their health: "))
+attack = int(input("Enter their attack: "))
+x = Enemies(Name, health, attack)
+enemies.append(x.__dict__)
+
+
+#No code needed below this line
+# Creates a new JSON file with the updated data
+new_file = "updated.json"
+with open(new_file, "w") as f:
+    # Serialize the updated Python list to a JSON string
+    json_string = json.dumps(enemies)
+
+    # Write the JSON string to the new JSON file
+    f.write(json_string)
+
+# Overwrite the old JSON file with the new one
+os.remove("enemies.json")
+os.rename(new_file, "enemies.json")
