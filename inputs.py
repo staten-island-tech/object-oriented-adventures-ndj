e926968ee4b7651fda668072d7d4014519e12505
NicholasB09 <158211547+NicholasB09@users.noreply.github.com>
2024-04-04 13:59:57 -0400

    wqer

--- inputs.py
+++ inputs.py
@@ -4,7 +4,7 @@ import screen
 class player_inputs():
 
     def __init__(self):
-        self.conquest = screen.game_screen("world_map.gif", "Conquest")
+        self.conquest = screen.create("world_map.gif", "Conquest")
         self.map = self.conquest.create(2600, 1400)
         self.conquest.text_input("How to use screens", "Utilize the scrollbars on the right and bottom of the screen to scroll up/down and left/right respectively. Go full screen for best experience.")
         
