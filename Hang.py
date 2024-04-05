5d2dd722e0040c91a7ee879a0bb7f799d236b5c1
JustinBregu <justinb612@nycstudents.net>
2024-04-05 13:18:55 -0400

    Update Hang.py

--- Hang.py
+++ Hang.py
@@ -1,1 +1,8 @@
-print("lasdikfhjgn")
+class Hangman():
+
+    def __init__(self, letters, count, death):
+        self.letters = letters    
+        self.count = count
+        self.death = death
+    def words():
+        word = ['across',  'against', 'laugh',  'middle',  'minute',  'mountain', 'ninth', 'ocean', 'office', 'parent' 'peanut' 'pencil' 'picnic' 'seconds' 'slowly' 'stories' 'student' 'sudden' 'suit' 'sure' 'swimming' 'though' 'threw' 'tired' well whole whose women wouldnt writing written wrote yell young']
