af1ebe473060415375b7266696fa69e15444471e
DanielT09 <158211608+DanielT09@users.noreply.github.com>
2024-04-05 13:50:44 -0400

    t

--- Boss.py
+++ Boss.py
@@ -4,16 +4,18 @@ import os
 
 class Enemies():
 
-    def __init__(self, Name, health, attack):
+    def __init__(self, Name, health, attack, continent):
         self.Name = Name
         self.health = health
         self.attack = attack
+        self.continent = continent
 
     def dictionary(self):
         Enemies = {
             'Name' : self.Name,
             'health' : self.health,
             'attack' : self.attack,
+            'continent': self.continent
         }
         return Enemies
 
@@ -26,7 +28,8 @@ enemies = json.load(f)
 Name = input("Enter an Enemies Name: ")
 health = int(input("Enter their health: "))
 attack = int(input("Enter their attack: "))
-x = Enemies(Name, health, attack)
+continent = input("Enter their continent: ")
+x = Enemies(Name, health, attack, continent)
 enemies.append(x.__dict__)
 
 
