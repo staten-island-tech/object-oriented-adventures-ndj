51a8e7627f288e9009bdbc6136ee2b44b5722f3a
JustinBregu <justinb612@nycstudents.net>
2024-04-08 13:58:48 -0400

    fhuiwqw

--- Hang.py
+++ Hang.py
@@ -1,23 +1,32 @@
-import turtle
-
 class Hangman():
 
-    def __init__(self, letters, count, death):
-        self.letters = letters    
-        self.count = count
-        self.death = death
-     
+    def inputs(self):
+        self.input = input("Welcome to the 'Hangman' minigame, are you ready to begin?: (Y/N)")
+        while self.input == 'Y':
+            print("Wow, I didn't think you were brave enough for this occasion...")
+        while self.input == 'N':
+            print("As I thought, you were not as brave as they all say...")
+
     def words(self):
         word = ['across',  'against', 'laugh',  'middle',  'minute',  'mountain', 'ninth', 'ocean', 'office', 'parent', 'peanut', 'pencil', 'picnic', 'seconds', 'slowly', 'stories', 'student', 'sudden', 'suit', 'sure', 'swimming', 'though', 'threw', 'tired', 'well', 'whole', 'whose', 'weird', 'wouldnt', 'writing', 'written', 'wrote', 'yell', 'young']
         letterss = ['a', 'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p']
         while letterss == word:
             if letterss == word:
                 print("You found the word!!!")
+            elif letterss != word:
+                self.count += 1
+                self.guesslist.append(self.guess)
             else:
-                print("You failed to guess the word!!!")
-    
-    
-    def create(self, width, height, screen):
-        screen = turtle.Screen()
-        screen.screensize(width, height)
-        screen.title(self.title)
+                print("You failed to guess to guess the number of nukes I have, now you die by my NUKES!!!")
+                self.death -= 1 
+
+    def tries(self):
+        self.count = 0 
+        self.guess = 0
+        self.guesslist = []
+  
+    def __init__(self, letters, count, death, lives):
+        self.letters = letters    
+        self.count = count
+        self.death = death
+        self.lives = lives
