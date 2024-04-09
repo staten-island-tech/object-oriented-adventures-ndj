24dcea6113c6a58c8c3837755e6901d526d6c409
JustinBregu <justinb612@nycstudents.net>
2024-04-09 12:39:18 -0400

    Update numguess.py

--- numguess.py
+++ numguess.py
@@ -4,12 +4,14 @@ joe_biden = print("Joe Biden wants you to guess how much nukes he has")
 
 number = random.randint(1, 20)
 guess = 0
-count = 0
+count = 1
 guesslist=[]
 while guess != number:
     guess = int(input("Enter Guess: "))
-
-    if guess < number:
+   
+    if count == 3:
+        print("You failed to guess to guess the number of nukes I have, now you die by my NUKES!!!")
+    elif guess < number:
         print("Guess higher!")
         count += 1
         guesslist.append(guess)
@@ -17,8 +19,6 @@ while guess != number:
         print("Guess lower!")
         count += 1
         guesslist.append(guess)
-    elif guess != number:
-        print("You failed to guess to guess the number of nukes I have, now you die by my NUKES!!!") 
     else:
         print("You won!")
         guesslist.append(guess)
