9e2108062744cee03847f8104b531ebd9f7ac270
JustinBregu <justinb612@nycstudents.net>
2024-04-09 13:02:15 -0400

    finished nuke guess

--- Hang.py
+++ Hang.py
@@ -1,12 +1,25 @@
+hangman_pic = "HANGMNA.gif"
+input = input("Welcome to the 'Hangman' minigame, are you ready to begin?: (Y/N)")
+while input == 'Y':
+    print("Wow, I didn't think you were brave enough for this occasion...")
+    a = hangman_pic = "HANGMNA.gif"
+while input == 'N':
+        print("As I thought, you were not as brave as they all say...")
+
+
 class Hangman():
 
     def inputs(self):
+        hangman_pic = "HANGMNA.gif"
         self.input = input("Welcome to the 'Hangman' minigame, are you ready to begin?: (Y/N)")
         while self.input == 'Y':
             print("Wow, I didn't think you were brave enough for this occasion...")
+            if self.input == 'Y':
+                return hangman_pic
         while self.input == 'N':
             print("As I thought, you were not as brave as they all say...")
-
+        
+        
     def words(self):
         word = ['across',  'against', 'laugh',  'middle',  'minute',  'mountain', 'ninth', 'ocean', 'office', 'parent', 'peanut', 'pencil', 'picnic', 'seconds', 'slowly', 'stories', 'student', 'sudden', 'suit', 'sure', 'swimming', 'though', 'threw', 'tired', 'well', 'whole', 'whose', 'weird', 'wouldnt', 'writing', 'written', 'wrote', 'yell', 'young']
         letterss = ['a', 'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p']
