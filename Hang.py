2432d033aa4c55dde6f0e67c1c86120c23ecd95a
JustinBregu <justinb612@nycstudents.net>
2024-04-18 13:58:56 -0400

    oka

--- Hang.py
+++ Hang.py
@@ -41,29 +41,40 @@ class Hangman():
 
 
 
+words = ['across',  'against', 'laugh',  'middle',  'minute',  'mountain', 'ninth', 'ocean', 'office', 'parent', 'peanut', 'pencil', 'picnic', 'seconds', 'slowly', 'stories', 'student', 'sudden', 'suit', 'sure', 'swimming', 'though', 'threw', 'tired', 'well', 'whole', 'whose', 'weird', 'wouldnt', 'writing', 'written', 'wrote', 'yell', 'young']
+word = random.choice(words)
+spaces = ['_']* len(word)
 
-word = ['across','against','laugh','middle','minute','mountain','ninth','ocean','office','parent','peanut','pencil','picnic','seconds','slowly','stories','student','sudden','suit','sure','swimming','though','threw','tired','well','whole','whose','weird','wouldnt','writing','written','wrote','yell','young']
-letterss = ['a', 'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-count = 1
-guess = 0 
-guesslist = []
-random_word = random.choice(word)
-entrance = input("Welcome to the 'Hangman' minigame, are you ready to begin?: (Y/N)")
-if input == 'Y':
-        print("Wow, I didn't think you were brave enough for this occasion...")
-if input == 'N':
-        print("As I thought, you were not as brave as they all say...")
-while random_word:    
-    print(random_word)
-    if letterss != random_word:
-        guess = input("Enter a letter to guess: ")
-        guesslist.append(guess)
-        count += 1  
-    elif count == 10:
-        print("As I thought... You proved worthless in this game of hangman..")
-    elif letterss == random_word:
-        print("You found the word!!!")
-        print('It took you', count, 'guesses!')
-        print('You can finally move on to the next mini-game...')
-        print(guesslist)
-        print(random_word)
+def get_letter_position(guess, word, spaces):
+    index = -2
+    while index != -1:
+        if guess in word:
+            index = word.find(guess)
+            removed_character ='*'
+            word = word[:index]+removed_character+word[index+1:]
+            spaces[index] = guess
+        else:
+            index = -1
+    return (word, spaces)
+
+def win_check():
+    for i in range(0, len(spaces)):
+        if spaces[i] == '_':
+            return -1
+     
+    return 1
+
+num_turns = len(word)
+for i in range(-1, num_turns):
+    guess = input('Guess a character:')
+    if guess in word:
+        word, spaces = get_letter_position(guess, word, spaces)
+        print(spaces)
+    else:
+        print('Sorry that letter is not in the word.')
+     
+    if win_check() == 1:
+        print("I didn't think that you were able to figure it out...")
+        break 
+    print('you have '+str(num_turns - i)+' turns left.')
+    print()
