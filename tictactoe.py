8682fd04b20a1408491984073810f275fbbc6327
DanielT09 <158211608+DanielT09@users.noreply.github.com>
2024-05-02 13:59:32 -0400

    qs

---
+++ tictactoe.py
@@ -0,0 +1,14 @@
+def class_tictactoe():
+    
+
+
+def tic_tac_toe():
+    print("I challenge you to a game of tic-tac-toe: ")
+    input(print("Pick x or o: "))
+    input(print("Pick your square out of "))
+
+
+
+
+
+tic_tac_toe()
