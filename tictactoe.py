be842dec65e55b5a68e3428d180ffd134ea3dd16
DanielT09 <158211608+DanielT09@users.noreply.github.com>
2024-05-03 14:00:02 -0400

    e

--- tictactoe.py
+++ tictactoe.py
@@ -1,14 +1,19 @@
-def class_tictactoe():
+Square_moves = ['A1,B1,C1,A2,B2,C2,A3,B3,C3']
     
-
 
 def tic_tac_toe():
     print("I challenge you to a game of tic-tac-toe: ")
-    input(print("Pick x or o: "))
-    input(print("Pick your square out of "))
+    g = input("Pick x or o: ")
+    if g == 'x':
+        print("You go first!")
+    if g == 'o':
+        print("You go after me!")
+    print("""
+        A1 / B1 / C1
+        A2 / B2 / C2
+        A3 / B3 / C3
+    """)
+    
 
-
-
-
-
+    
 tic_tac_toe()
