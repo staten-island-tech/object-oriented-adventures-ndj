acd0de3719b45ad7590c345ee1d726a8948be0f1
DanielT09 <158211608+DanielT09@users.noreply.github.com>
2024-05-06 14:02:17 -0400

    t

--- tictactoe.py
+++ tictactoe.py
@@ -1,18 +1,43 @@
-Square_moves = ['A1,B1,C1,A2,B2,C2,A3,B3,C3']
+Square_moves = ['A1','B1','C1','A2','B2','C2','A3','B3','C3']
+A1 = 'A1'
+B1 = 'B1'
+C1 = 'C1'
+A2 = 'A2'
+B2 = 'B2'
+C2 = 'C2'
+A3 = 'A3'
+B3 = 'B3'
+C3 = 'C3'
+
+change_squares = [A1]
     
 
 def tic_tac_toe():
+    a = True
     print("I challenge you to a game of tic-tac-toe: ")
     g = input("Pick x or o: ")
-    if g == 'x':
-        print("You go first!")
-    if g == 'o':
-        print("You go after me!")
-    print("""
-        A1 / B1 / C1
-        A2 / B2 / C2
-        A3 / B3 / C3
-    """)
+    while a:
+        print(f"""
+            {A1} / {B1} / {C1}
+            {A2} / {B2} / {C2}
+            {A3} / {B3} / {C3}
+            """)
+        if g == 'x':
+            print("You go first!")
+            square = input("Pick a square: ")
+            if square not in Square_moves:
+                print("You are acoustic")
+            else:
+                print("Good move")
+                for i in change_squares:
+                    if square == str(i):
+                        i = g
+                        print(i)
+
+        if g == 'o':
+            print("You go after me!")
+    
+    
     
 
     
