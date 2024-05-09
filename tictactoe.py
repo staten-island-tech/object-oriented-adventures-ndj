1f88f81ffb8c55674130fbc794196b2ece92db89
DanielT09 <158211608+DanielT09@users.noreply.github.com>
2024-05-09 14:00:04 -0400

    tr

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,4 @@
-
+import random
     
 
 def tic_tac_toe():
@@ -13,16 +13,16 @@ def tic_tac_toe():
     B3 = 'B3'
     C3 = 'C3'
 
-    change_squares = [A1,B1,C1,A2,B2,C2,A3,B3,C3]
+    change_squares = ['A1', 'B1', 'C1', 'A2','B2', 'C2', 'A3', 'B3', 'C3']
+    taken_squares = []
     a = True
     print("I challenge you to a game of tic-tac-toe: ")
     g = input("Pick x or o: ")
     while a:
-        print(A1)
         print(f"""
-            {A1} / {B1} / {C1}
-            {A2} / {B2} / {C2}
-            {A3} / {B3} / {C3}
+            {change_squares[0]} / {change_squares[1]} / {change_squares[2]}
+            {change_squares[3]} / {change_squares[4]} / {change_squares[5]}
+            {change_squares[6]} / {change_squares[7]} / {change_squares[8]}
             """)
         if g == 'x':
             print("You go first!")
@@ -31,15 +31,21 @@ def tic_tac_toe():
                 print("You are acoustic")
             else:
                 print("Good move")
+                a = 0
                 for i in change_squares:
                     if square == str(i):
-                        print(i)
                         i = g
-                        print(i)
-                        print(A1)
+                        taken_squares.append(a)
+                        change_squares[a] = i
+                        
+                    a += 1
 
         if g == 'o':
             print("You go after me!")
+            random_square = random.randint(0, 8)
+            if random_square not in taken_squares:
+                change_squares[random_square] = 'x'
+            
     
     
     
