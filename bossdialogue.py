dbea8638291deacc7bb3e5adb4aa1177a8d3378c
JustinBregu <justinb612@nycstudents.net>
2024-05-16 13:57:41 -0400

    oa

---
+++ bossdialogue.py
@@ -0,0 +1,17 @@
+import random
+
+class BD:
+
+    def Putin():
+        Putin_says = input("You are the one that they were all talking about huh?: (Y/N)")
+        if Putin_says == 'Y':
+            print("Тебе предстоит непростое испытание, молодой человек --> Translator: Mr. Putin said you are up for quite a challenge young one...")
+        if Putin_says == 'N':
+            print("Тогда ты бесполезен, молодой --> Translator: Mr. Putin said that you are worthless young one...")
+    
+    def Joe_Biden():
+        Joe_Biden_says = input (" *cough cough* I didn't didn't expect for you ice cream to meet me at this moment: (What the hell are you saying?/Your better off dying old man!)")
+        if Joe_Biden_says == 'What the hell are you saying?':
+            print("Oh, my dementa is kicking in.. GuESS i NEED to kick your ass right... right... now!")
+        if Joe_Biden_says == "Your better off dying old man!":
+            print("RESPECT YO-YOUR ELDERSS!!! NOW IM GOING TO KI--KILL Y-YOUU!!")
