e0134d9c3f799fe0e11b1f7af1b60105c5b5a70b
JustinBregu <justinb612@nycstudents.net>
2024-05-17 13:59:59 -0400

    Update bossdialogue.py

--- bossdialogue.py
+++ bossdialogue.py
@@ -3,15 +3,46 @@ import random
 class BD:
 
     def Putin():
-        Putin_says = input("You are the one that they were all talking about huh?: (Y/N)")
+        Putin_says = input("You are the one that they were all talking about... huh?: (Y/N)")
         if Putin_says == 'Y':
             print("Тебе предстоит непростое испытание, молодой человек --> Translator: Mr. Putin said you are up for quite a challenge young one...")
         if Putin_says == 'N':
             print("Тогда ты бесполезен, молодой --> Translator: Mr. Putin said that you are worthless young one...")
     
     def Joe_Biden():
-        Joe_Biden_says = input (" *cough cough* I didn't didn't expect for you ice cream to meet me at this moment: (What the hell are you saying?/Your better off dying old man!)")
+        Joe_Biden_says = input (" *cough cough* I didn't didn't expect for you ice cream to meet me at this moment (What the hell are you saying?/Your better off dying old man!): ")
         if Joe_Biden_says == 'What the hell are you saying?':
             print("Oh, my dementa is kicking in.. GuESS i NEED to kick your ass right... right... now!")
         if Joe_Biden_says == "Your better off dying old man!":
             print("RESPECT YO-YOUR ELDERSS!!! NOW IM GOING TO KI--KILL Y-YOUU!!")
+
+    def Hugo_Chavez():
+        Hugo_Chavez_says = input("No te acerques a mí, sucio pedazo de mierda. --> Translator: Mr. Chavez said that you must get away you dirty piece of crap.. (I came here to kill him, not to run away/Alright, well play by your rules): ")
+        if Hugo_Chavez_says == 'I came here to kill him, not to run away':
+            print("You must have some courage trying to fight me one on one, huh?!")
+        if Hugo_Chavez_says == 'Alright, well play by your rules':
+            print("Eso es lo que pensé... ¡Vagabundo! --> Translator: Mr. Chavez said thats what I thought you bum!")
+    
+    def Albert_Frick():
+        Albert_Frick_says = input("Be fr with me, am I cute? (HELL NO/You are one of the ugliest creature I have seen!): ")
+        if Albert_Frick_says == 'HELL NO':
+            print("I'll prove you wrong, you dirty mole rat!")
+        if Albert_Frick_says == 'You are one of the ugliest creature I have seen!':
+            print("During our battle, I will make sure you feel my wrath !!!!!")
+
+    def Ashraf():
+        Ashraf_says = input("May praise be with Allah, Allahu Akbar! (As-salamu alaykum/Christianity is the true religion!!)")
+        if Ashraf_says == 'As-salamu alaykum':
+            print("Wa-Alaikum-Salaam")
+        if Ashraf_says == 'Christianity is the true religion!!':
+            print('Well fight over Jerusalem then, just as King Baldwin did!')
+
+    def Felix():
+        Felix_says = input("Gracias por reunirse conmigo hoy - ¿Cuáles son sus planes --> Translator: President Felix is thanking you for meeting with him, and what your plans are... (I want to fight you, Mr. Felix/I appreciate you being present, and I have a gift for you!): ")
+        if Felix_says == 'I want to fight you, Mr. Felix':
+            print("Okay then youngling, we shall present a duel between the both of us")
+        if Felix_says == 'I appreciate you being present, and I have a gift for you!':
+            print("The gift is the upbringing of death, I already know it!")
+    
+    def Zelensky():
+        Zelensky_says = input("Here in Ukraine, we have been waiting for you...Now choose to ally or fight with us! (I h) ")
